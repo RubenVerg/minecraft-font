f5bf2340a452579fd49771b47dccd0d3df0fdaf5
Glitch (hello-webpage) <none>
2020-06-23 14:06:14 +0000

    🌏🍘 Checkpoint ./main.js:8129470/1774 ./.glitch-assets:8129470/1577

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const E_Statuses = {
   sure: 0,
   probably: 1,
   notsure: 2,
-  closest: 3
+  closest: 3,
+  unknown: 4
 };
 
 const I_Character = {
@@ -25,24 +26,28 @@ const sizes = {
 }
 
 const urls = {
-  ascii: 'https://cdn.glitch.com/cbbdf2fb-da12-4b7b-964d-e54c67324efe%2Fascii.png?v=1592920273925',
-  accented: 'https://cdn.glitch.com/cbbdf2fb-da12-4b7b-964d-e54c67324efe%2Faccented.png?v=1592920274411',
-  nonlatin_european: 'https://cdn.glitch.com/cbbdf2fb-da12-4b7b-964d-e54c67324efe%2Fnonlatin_european.png?v=1592920274319',
-  ascii_sga: 'https://cdn.glitch.com/cbbdf2fb-da12-4b7b-964d-e54c67324efe%2Fascii_sga.png?v=1592920274130'
+  ascii: 'https://cdn.glitch.com/cbbdf2fb-da12-4b7b-964d-e54c67324efe%2Fascii.png',
+  accented: 'https://cdn.glitch.com/cbbdf2fb-da12-4b7b-964d-e54c67324efe%2Faccented.png',
+  nonlatin_european: 'https://cdn.glitch.com/cbbdf2fb-da12-4b7b-964d-e54c67324efe%2Fnonlatin_european.png',
+  ascii_sga: 'https://cdn.glitch.com/cbbdf2fb-da12-4b7b-964d-e54c67324efe%2Fascii_sga.png'
 }
 
-const qsAll = (...args) => document.querySelectorAll(...args);
-
-const qsOne = (...args) => document.querySelector(...args);
+const urlsBlack = {
+  ascii: 'https://cdn.glitch.com/cbbdf2fb-da12-4b7b-964d-e54c67324efe%2Fascii_black.png',
+  accented: 'https://cdn.glitch.com/cbbdf2fb-da12-4b7b-964d-e54c67324efe%2Faccented_black.png',
+  nonlatin_european: 'https://cdn.glitch.com/cbbdf2fb-da12-4b7b-964d-e54c67324efe%2Fnonlatin_european_black.png',
+  ascci_sga: 'https://cdn.glitch.com/cbbdf2fb-da12-4b7b-964d-e54c67324efe%2Fascii_sga_black.png'
+}
 
 function popup(charFile, charX, charY, characters) {
-  qsOne("#popupTitle").textContent = files[charFile];
+  document.querySelector("#popupTitle").textContent = files[charFile];
   
-  qsOne('#popupMc').style.display = 'inline-block';
-  qsOne('#popupMC').style.backgroundImage = 'url("/' + urls[charFile] + '.png")';
-  qsOne('#popupMc').style.backgroundPosition = sizes[charFile][0] * charX + ' ' + sizes[charFile][1] * charY;
-  qsOne('#popupMc').style.width = sizes[charFile][0];
-  qsOne('#popupMc').style.height = sizes[charFile][1];
+  console.log(document.querySelector('#popupMc'))
+  document.querySelector('#popupMc').style.display = 'inline-block';
+  document.querySelector('#popupMc').style.backgroundImage = 'url("' + urls[charFile] + '")';
+  document.querySelector('#popupMc').style.backgroundPosition = sizes[charFile][0] * charX + ' ' + sizes[charFile][1] * charY;
+  document.querySelector('#popupMc').style.width = sizes[charFile][0];
+  document.querySelector('#popupMc').style.height = sizes[charFile][1];
 
   $("#popupModal").modal();
 }
