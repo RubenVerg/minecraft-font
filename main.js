41684d5cc5c46679c9b21bdf31a48d4b7cdea9d5
Glitch (hello-webpage) <none>
2020-06-23 13:42:15 +0000

    🍈👏 Checkpoint ./index.html:8129470/5432 ./main.js:8129470/2044 ./style.scss:8129470/96

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-function popup() {}
+const E_Statuses = {
+  sure: 0,
+  probably: 1,
+  notsure: 2,
+  closest: 3
+};
+
+const I_Character = {
+  char: "\u{0}",
+  status: E_Statuses.sure
+};
+
+const files = {
+  ascii: "ASCII",
+  accented: "Accented",
+  nonlatin_european: "Nonlatin European",
+  ascii_sga: "ASCII (Standard Galactic Alphabet)"
+};
+
+const sizes = {
+  ascii: []
+}
+
+const qsAll = (...args) => document.querySelectorAll(...args);
+
+const qsOne = (...args) => document.querySelector(...args);
+
+function popup(charFile, charX, charY, characters) {
+  qsOne("#popupTitle").textContent = files[charFile];
+  
+  qsOne('#popupMc').src = '/' + files[charFile] + '.png'
+
+  $("#popupModal").modal();
+}
